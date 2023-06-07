1da9553a89d33d1a1467a3783c2327a1e837d30a
Kushaal Manchella <35774289+kushaal-manchella@users.noreply.github.com>
2023-06-07 12:19:10 -0500

    Update Qatester.java

--- employee/src/main/java/com/example/employee/model/Qatester.java
+++ employee/src/main/java/com/example/employee/model/Qatester.java
@@ -1,20 +1,12 @@
 package com.example.employee.model;
-import jakarta.persistence.DiscriminatorValue;
+
 import jakarta.persistence.Entity;
-import java.sql.Date;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
-@DiscriminatorValue("qatester")
+@JsonTypeName("qatester")
 public class Qatester extends Employee{
-	public Qatester() {};
-	
-	public Qatester(Long employeeId, String name, Date dob, String email, int salary, Long reportingManager, String department) {
-		super.setEmployeeId(employeeId);
-		super.setName(name);
-		super.setDob(dob);
-		super.setEmailId(email);
-		super.setSalary(salary);
-		super.setReportingManager(reportingManager);
-		super.setDepartment(department);
-	}
+	public Qatester() {
+		super.setRole("employee");
+	};
 }
