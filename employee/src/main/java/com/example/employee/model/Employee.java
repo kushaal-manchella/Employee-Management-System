6f12d1b75e91988a846e644ff90321e72ba7303a
Kushaal Manchella <35774289+kushaal-manchella@users.noreply.github.com>
2023-06-07 12:18:13 -0500

    Update Employee.java
    Updating employee model

--- employee/src/main/java/com/example/employee/model/Employee.java
+++ employee/src/main/java/com/example/employee/model/Employee.java
@@ -1,13 +1,16 @@
 package com.example.employee.model;
 
-
 import java.sql.Date;
-
+import com.example.exceptions.InvalidInputException;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorType;
@@ -15,8 +18,15 @@ import jakarta.persistence.DiscriminatorType;
 
 
 @Entity
-@DiscriminatorColumn(name = "role", discriminatorType = DiscriminatorType.STRING)
 @Table(name = "employee")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "role", discriminatorType = DiscriminatorType.STRING)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "role")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = Developer.class, name = "developer"),
+    @JsonSubTypes.Type(value = Manager.class, name = "manager"),
+    @JsonSubTypes.Type(value = Qatester.class, name = "qatester")
+})
 public abstract class Employee {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,9 +47,12 @@ public abstract class Employee {
     
     @Column(name="reporting_manager")
     private Long reportingManager;
-    
-    @Column(name="department")
+
+	@Column(name="department")
     private String department;
+    
+    @Column(name="role", insertable = false, updatable = false)
+    private String role;
     
 	public Long getEmployeeId() {
 		return employeeId;
@@ -96,7 +109,34 @@ public abstract class Employee {
 	public void setDepartment(String department) {
 		this.department = department; 
 	}
+	
+    public String getRole() {
+		return role;
+	}
 
+	public void setRole(String role) {
+		this.role = role;
+	}
 
+	public void validate() {
+		if(this.name == null) {
+			throw new InvalidInputException("Please provide a name field");
+		}
+		else if (this.dob == null) {
+			throw new InvalidInputException("Please provide a dob field");
+		}
+		else if (this.emailId == null) {
+			throw new InvalidInputException("Please provide an emailId field");
+		}
+		else if (this.salary == 0) {
+			throw new InvalidInputException("Please provide a salary field");
+		}
+		else if (this.department == null) {
+			throw new InvalidInputException("Please provide a department field");
+		}
+		else if (!"manager".equals(this.role) && !"qatester".equals(this.role) && !"developer".equals(this.role)) {
+			throw new InvalidInputException("Invalid role. Role must be either manager, developer or qatester");
+		}
+	}
 }
 
